111c7cbb80aca63b9f141f1a039dd4f10e2a3acf
laila <100658865+laysch@users.noreply.github.com>
2023-05-09 12:19:05 -0400

    Update and rename UnoGame.java to Uno.java

---
+++ Uno.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.uno;
+
+/**
+ *
+ * @author laila
+ */
+public class Uno {
+
+    public static void main(String[] args)throws Exception {
+         new Menu().setVisible(true);
+    }
+}
